ec49d1925e998982280985da7e1dc663622e1666
Nathan Turner <nathan@clicksminuteper.net>
2021-08-10 10:04:49 +0100

    fix site

--- docs/_next/static/PI2azvPuEaz9Z0jWtyH-q/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/64e4a3e648ef973bd5f4.css","static/chunks/pages/index-637cc0db418d7a1975ba.js"],"/_error":["static/chunks/pages/_error-a0e21b9b223f827fe1f2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
